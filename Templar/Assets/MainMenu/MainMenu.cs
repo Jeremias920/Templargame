11cb9926951ccd6feb6b99f20723cac902877a62
Ville Lapakko <ville.lapakko@hotmail.com>
2014-03-10 10:44:50 +0200

    Added Main Menu for Build

---
+++ Templar/Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+	public GUIStyle PlayGame;
+	public GUIStyle Options;
+	public GUIStyle Quit;
+
+	void OnGUI(){
+				//Display our BGTexture
+				GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+				//Display buttons
+				if (GUI.Button (new Rect (Screen.width * .40f, Screen.height * .5f, Screen.width * .2f, Screen.height * .1f), "", PlayGame)) {
+			Application.LoadLevel("Testi");
+
+				}
+
+				if (GUI.Button (new Rect (Screen.width * .40f, Screen.height * .6f, Screen.width * .2f, Screen.height * .1f), "", Options)) {
+			
+			
+				}
+				if (GUI.Button (new Rect (Screen.width * .40f, Screen.height * .7f, Screen.width * .2f, Screen.height * .1f), "", Quit)) {
+			Application.Quit();
+			
+				}
+		}
+
+}
